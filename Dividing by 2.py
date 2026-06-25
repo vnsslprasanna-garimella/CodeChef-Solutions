b40738fe010f511fd87cc008449d3b6125699218
prasanna <gvnsslprasanna@gmail.com>
2026-06-25 11:44:09 +0530

    Added Dividing by 2 solution

---
+++ Dividing by 2.py
@@ -0,0 +1,33 @@
+# cook your dish here
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    cnt = {}
+    total = {}
+
+    for x in a:
+        steps = 0
+        while True:
+            if x not in cnt:
+                cnt[x] = 0
+                total[x] = 0
+
+            cnt[x] += 1
+            total[x] += steps
+
+            if x == 0:
+                break
+
+            x //= 2
+            steps += 1
+
+    ans = float('inf')
+
+    for v in cnt:
+        if cnt[v] == n:
+            ans = min(ans, total[v])
+
+    print(ans)
